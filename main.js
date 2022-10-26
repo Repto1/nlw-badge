74f17d6394f2e3667942db75381bed5a231bc7ad
João Costa <joaovitorcostaoliveira@gmail.com>
2022-10-26 18:46:28 -0300

    api fix

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ function changeSocialMediaLinks() {
 changeSocialMediaLinks()
 
 function getGithubProfileInfos() {
-  const url = `https://api.github.com/user/${linksSocialMedia.github}`
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
       userName.textContent = data.name
       userBio.textContent = data.bio
-      userLink.href = data.html_url
+      userLink.href = data.user_url
       userImage.src = data.avatar_url
       userLogin.textContent = data.login
     })
